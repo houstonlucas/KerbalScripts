983264bc293a229e76dae5c6d6fec8f5574eda8b
houstonlucas <houstonlucas@nevada.unr.edu>
2017-10-29 18:31:31 -0700

    Continued creating tools for rendezvous.

--- Scripts/OrbitTools.py
+++ Scripts/OrbitTools.py
@@ -1,6 +1,7 @@
 import time
 import math
 import krpc
+from RendezvousTools import *
 
 e = math.e
 
@@ -49,6 +50,54 @@ class OrbitTools:
             dp *= 0.8
         nd.prograde = offset
 
+    def create_period_modification_node(self, dp, threshold, new_period, anomaly):
+        ut = self.vessel.orbit.ut_at_true_anomaly(anomaly)
+        nd = self.vessel.control.add_node(ut, 0.0)
+        offset = 0.0
+        best = 999999999
+        while best > threshold:
+            nd.prograde = offset - dp
+            e1 = abs(nd.orbit.period - new_period)
+            nd.prograde = offset + dp
+            e2 = abs(nd.orbit.period - new_period)
+            if e1 < e2:
+                offset -= dp
+                best = e1
+            else:
+                offset += dp
+                best = e2
+            dp *= 0.8
+        nd.prograde = offset
+
+    # Creates a node that matches your inclination with the target.
+    def create_inclination_matching_node(self, dn, threshold):
+        target = self.ksc.target_vessel
+
+        # Decide which is closer ascending or descending node.
+        anomaly_dn = self.vessel.orbit.true_anomaly_at_dn(target)
+        anomaly_an = self.vessel.orbit.true_anomaly_at_an(target)
+        ut_at_dn = self.vessel.orbit.ut_at_true_anomaly(anomaly_dn)
+        ut_at_an = self.vessel.orbit.ut_at_true_anomaly(anomaly_an)
+        ut_at_node = min(ut_at_an, ut_at_dn)
+
+        # Create Node
+        nd = self.vessel.control.add_node(ut_at_node, 0.0)
+        offset = 0.0
+        best = 999999999
+        while best > threshold:
+            nd.normal = offset - dn
+            diff1 = nd.orbit.relative_inclination(target)
+            nd.normal = offset + dn
+            diff2 = nd.orbit.relative_inclination(target)
+            if diff1 < diff2:
+                offset -= dn
+                best = diff1
+            else:
+                offset += dn
+                best = diff2
+            dn *= 0.8
+        nd.normal = offset
+
     # TODO have this account for partial stages.
     def get_burn_time(self, dv):
         body = self.vessel.orbit.body
@@ -62,28 +111,37 @@ class OrbitTools:
         burn_time = m_prop / m_dot
         return burn_time
 
-    def execute_next_node(self, remove_after=True):
+    # TODO have this account for partial stages.
+    # Executes the next maneuver node.
+    #  Variables:
+    #  remove_after: Bool indicating if the node should be removed after execution.
+    #  buffer_time: Amount of time before node execution starts to stop time warp.
+    #  slow_burn_time: How much of the burn is reserved for slow burn.
+    # Assumptions:
+    #  Relative Inclination near zero
+    def execute_next_node(self, remove_after=True, buffer_time=10.0, slow_burn_time=0.5):
         nd = self.vessel.control.nodes[0]
         burn_direction = nd.direction(self.vessel.surface_reference_frame)
         self.vessel.auto_pilot.target_direction = burn_direction
         self.vessel.auto_pilot.engage()
-        # vessel.auto_pilot.wait()
+
         dv = nd.delta_v
         burn_time = self.get_burn_time(dv)
         print("Expecting {}s burn.".format(burn_time))
         print("Warping")
-        self.ksc.warp_to(nd.ut - burn_time / 2.0)
+        self.ksc.warp_to(nd.ut - (burn_time / 2.0) - buffer_time)
         burn_direction = nd.direction(self.vessel.surface_reference_frame)
         self.vessel.auto_pilot.target_direction = burn_direction
-        self.vessel.control.throttle = 1.0
+        time.sleep(buffer_time)
 
-        # Perform most of the burn
-        while self.get_burn_time(nd.remaining_delta_v) > 1.0:
-            time.sleep(0.2)
+        self.vessel.control.throttle = 1.0
+        # Perform most of the burn, stopping when half a second is remaining.
+        while self.get_burn_time(nd.remaining_delta_v) > slow_burn_time:
+            time.sleep(0.1)
             burn_direction = nd.remaining_burn_vector(self.vessel.surface_reference_frame)
             self.vessel.auto_pilot.target_direction = burn_direction
 
-        # Throttle down
+        # Start slow burn
         self.vessel.control.throttle = 0.1
         # Home in on remainder of burn.
         while nd.remaining_delta_v > 0.5:
@@ -97,7 +155,7 @@ class OrbitTools:
             nd.remove()
 
     def stage_if_needed(self, ):
-        resources_in_stage = self.vessel.resources_in_decouple_stage(self.vessel.control.current_stage-1, True)
+        resources_in_stage = self.vessel.resources_in_decouple_stage(self.vessel.control.current_stage - 1, True)
         fuel_list = filter(lambda item: item.name in ["LiquidFuel", "SolidFuel"], resources_in_stage.all)
         fuel_in_stage = sum(item.amount for item in fuel_list)
         if fuel_in_stage < 0.05:
@@ -110,13 +168,76 @@ class OrbitTools:
         time.sleep(0.1)
 
     def wait_until_drag_is_negligible(self, ):
-        while get_magnitude(self.vessel.flight().drag) > 0.05:
+        while get_magnitude(self.vessel.flight().drag) > 0.005:
             time.sleep(1)
 
     def get_ascent_angle(self, ):
         altitude_ratio = self.altitude() / self.ascent_angle_flatten_alt
         return max(5, 90 - (90 * altitude_ratio))
 
+    def rendezvous_with_target(self, max_number_of_orbits=10):
+        target = self.ksc.target_vessel
 
-def get_magnitude(x):
-    return math.sqrt(sum([val * val for val in x]))
+        # Match inclinations
+        self.create_inclination_matching_node(1000, 0.0001)
+        self.execute_next_node()
+
+        # Adjust period for rendezvous
+        t = self.vessel.orbit.period
+        target_t = target.orbit.period
+        target_lag = calculate_target_lag(self.vessel, target)
+
+        mu = self.vessel.orbit.body.gravitational_parameter
+        new_period = find_optimal_rendevous_period(t, target_t, target_lag, mu, range(1, max_number_of_orbits))
+        intersect_ut = get_next_approach(self.vessel, target)
+        intersect_anomaly = self.vessel.orbit.true_anomaly_at_ut(intersect_ut)
+
+        self.create_period_modification_node(1000, 0.001, new_period, intersect_anomaly)
+        self.execute_next_node()
+
+    def zero_relative_velocity(self, target, buffer_time=5.0):
+        frame = self.vessel.surface_reference_frame
+
+        initial_threshold = 10.0
+        medium_threshold = 2.0
+        final_threshold = 0.1
+        relative_v = target.velocity(frame)
+
+        self.vessel.auto_pilot.target_direction = relative_v
+        self.vessel.auto_pilot.engage()
+        time.sleep(buffer_time)
+        self.vessel.control.throttle = 1.0
+        while get_magnitude(relative_v) > initial_threshold:
+            relative_v = target.velocity(frame)
+            self.vessel.auto_pilot.target_direction = relative_v
+
+        # Start medium burn
+        self.vessel.control.throttle = 0.1
+        while get_magnitude(relative_v) > medium_threshold:
+            relative_v = target.velocity(frame)
+            self.vessel.auto_pilot.target_direction = relative_v
+
+        # Start slow burn
+        self.vessel.control.throttle = 0.01
+        while get_magnitude(relative_v) > final_threshold:
+            relative_v = target.velocity(frame)
+            self.vessel.auto_pilot.target_direction = relative_v
+
+        # Throttle Off
+        self.vessel.control.throttle = 0.0
+
+    def burn_towards_target(self, target, vel):
+        frame = self.vessel.surface_reference_frame
+
+        relative_pos = target.position(frame)
+
+        self.vessel.auto_pilot.target_direction = relative_pos
+        self.vessel.auto_pilot.engage()
+        self.vessel.auto_pilot.wait()
+
+        burn_time = self.get_burn_time(vel)
+        self.vessel.control.throttle = 1.0
+        time.sleep(burn_time)
+        self.vessel.control.throttle = 0.0
+
+
--- Scripts/RendezvousTools.py
+++ Scripts/RendezvousTools.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, sqrt
 
 
 # Returns the orbital period that will intersect the target in n of the targets orbits.
@@ -30,18 +30,35 @@ def calculate_new_period(period, targets_period, target_lag, n):
 # Assumptions:
 # Relative Inclination near zero
 #
-def find_optimal_rendevous_orbit(period, target_period, target_lag, mu, valid_n=range(1, 10)):
+def find_optimal_rendevous_period(period, target_period, target_lag, mu, valid_n=range(1, 5)):
     # Iterate over valid_n, choose n that minimizes the dv needed to change orbital period.
     v = velocity_from_semi_major(semi_major_from_period(period, mu), mu)
-    potential__t = map(lambda n: calculate_new_period(period, target_period, target_lag, n), valid_n)
-    potential_a = map(lambda t_new: semi_major_from_period(t_new, mu), potential__t)
+    potential_period = map(lambda n: calculate_new_period(period, target_period, target_lag, n), valid_n)
+    potential_a = map(lambda t_new: semi_major_from_period(t_new, mu), potential_period)
     potential_v = map(lambda a: velocity_from_semi_major(a, mu), potential_a)
     dvs = map(lambda vn: abs(v - vn), potential_v)
     dv = min(dvs)
     index = dvs.index(dv)
     print("{} orbits to intersect.".format(valid_n[index]))
-    return potential__t[index]
+    return potential_period[index]
 
+
+def calculate_target_lag(vessel, target):
+    # The ut that we will use for the rendezvous setup burn
+    vessel_approach_ut = get_next_approach(vessel, target)
+
+    # Only works if we make the assumption that the orbits are similar in inclination
+    # and that the longitudes of ascending nodes are also similar
+    periapsis_offset = vessel.orbit.argument_of_periapsis - target.orbit.argument_of_periapsis
+    anomaly_intersect = vessel.orbit.true_anomaly_at_ut(vessel_approach_ut)
+    target_intersect_anomaly = anomaly_intersect + periapsis_offset
+
+    target_approach_ut = target.orbit.ut_at_true_anomaly(target_intersect_anomaly)
+    lag = (target_approach_ut - vessel_approach_ut) % target.orbit.period
+    if lag > target.orbit.period/2.0:
+        # If they are lagging by more than half an orbit then calculate it in front instead of behind.
+        lag = target.orbit.period - lag
+    return lag
 
 def semi_major_from_period(t, mu):
     return ((t * t * mu) / (4.0 * pi * pi)) ** (1.0 / 3.0)
@@ -50,3 +67,19 @@ def semi_major_from_period(t, mu):
 # Currently an approximation
 def velocity_from_semi_major(a, mu):
     return (mu / a) ** 0.5
+
+
+def get_next_approach(vessel, target):
+    return vessel.orbit.list_closest_approaches(target, 1)[0][0]
+
+
+def get_magnitude(x):
+    return sqrt(sum([val * val for val in x]))
+
+
+def get_difference(x, y):
+    return [x[i] - y[i] for i in range(len(x))]
+
+
+def scale(x, s):
+    return [a * s for a in x]
