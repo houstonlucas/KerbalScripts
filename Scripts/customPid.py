df2af4f61950e7fdd2db110f0823b8215df4ff22
Houston Lucas <houstonlucas@nevada.unr.edu>
2019-01-27 20:56:43 -0800

    Added PIDController

---
+++ Scripts/customPid.py
@@ -0,0 +1,28 @@
+
+class PidController:
+    def __init__(self, gains=(0.0, 0.0, 0.0),
+                 effort_bounds=(0.0, 0.0),
+                 integral_threshold=0.25):
+        self.k_p, self.k_i, self.k_d = gains
+        self.e_prev, self.i_prev = None, 0.0
+        self.min_effort, self.max_effort = effort_bounds
+        self.integration_threshold = integral_threshold
+
+    def get_effort(self, target, measurement, dt):
+        if self.e_prev is None:
+            self.e_prev = target - measurement
+
+        e = target - measurement
+        p = e
+        i = self.i_prev + ((e + self.e_prev) * dt / 2.0)
+        d = (e - self.e_prev) / dt
+
+        if not (-self.integration_threshold < e < self.integration_threshold):
+            i = 0.0
+
+        self.e_prev = e
+        self.i_prev = i
+
+        effort = self.k_p * p + self.k_i * i + self.k_d * d
+        effort = max(min(effort, self.max_effort), self.min_effort)
+        return effort
